47449690e0e719673796e5d5f1fa64472bfc4f6a
Nanjiang Shu <nanjiang.shu@bils.se>
2017-10-23 11:28:49 +0200

    search for credfile in system

--- src/get_egi_token.sh
+++ src/get_egi_token.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+credfile=$(find /tmp/ -maxdepth 1 -name "x509up_*" )
+if [ "$credfile" == "" ];then
+    echo "credfile not found at /tmp" >&2
+    exit 1
+fi
 
-curl  -k  --cert /tmp/x509up_u1000 -d '{"auth":{"voms": true, "tenantName": "EGI_bils"}}' -H "Content-type: application/json" https://cloud.recas.ba.infn.it:5000/v2.0/tokens 2>& 1 | tr '{' '\n' | grep 'id' | head -n 1 | grep 'id'
+curl  -k  --cert $credfile  -d '{"auth":{"voms": true, "tenantName": "EGI_bils"}}' -H "Content-type: application/json" https://cloud.recas.ba.infn.it:5000/v2.0/tokens 2>& 1 | tr '{' '\n' | grep 'id' | head -n 1 | grep 'id'
 
